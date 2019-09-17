b49c31324ebb4929b868b3c827950a41301877cb
Sudhir Kumar Sharma <sudhir.kumar@crmnext.in>
2019-09-17 15:02:52 +0530

    React Component with Router and SPA
    React Router
    Functional Compnent
    Router
    Router with parameter
    SPA

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,34 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import './App.css';
+import React  ,{ Component } from 'react';
+ import './App.css';
+//import Menu from './components/MenuComponent';
+//import TravelMenu from './components/TravelComponent';
+import Main from './components/maincomponent';
+import {Navbar, NavbarBrand } from 'reactstrap';
+import Home from './components/Homepage';
+import MenuCard from './components/MenuCardComponent';
+import Menu from './components/MenuComponent';
+//import MenuCard from './components/MenuCardComponent';
+ //import { DISHES } from './shared/dishes';
+//import {Navbar, NavbarBrand } from 'reactstrap';
+//import Tutor from './components/TutorComponent';
+//import DishDetail from './components/DishdetailComponent';
+import {  BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-<Navbar dark-color="primary">
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+    
+  //}
 
-<div className="container">
-<NavbarBrand href="/">Sudhir first website </NavbarBrand>
 
-</div>
-</Navbar>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Check and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
-  );
-}
 
-export default App;
+  render() {
+    return (
+      <BrowserRouter>
+      <div>
+        {/* <Home  /> */}
+      <Main  />
+      </div>
+      </BrowserRouter>
+
+      );
+  }
+   }
+   export default App;
+
+
+
+
+
+//function App() {
+//  class App extends Component {
+
+
+
+
+// render() {
+//   return (
+//     <div><Main  /></div>
+//     );
+// }
+//  }
+//  export default App;
+
+ {/* <Navbar dark color="secondary">
+   <div className="container">
+   <NavbarBrand href="/">React</NavbarBrand>   
+   </div>
+   </Navbar> */}
+   {/* <DishDetail  dishes={this.state.dishes} /> */}
+  {/* <TravelMenu /> */}
+    {/* <MenuCard dishes={this.state.dishes} />   */}
+    {/* <div ><Tutor /> </div> */}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function App1() {
+//   render() {
+//   return (
+//     // <div className="App">
+//     <div >
+//  <Navbar dark color="secondary">
+//    <div className="container">
+//    <NavbarBrand href="/">Travel </NavbarBrand>   
+ 
+//    </div>
+//    </Navbar>
+   
+//     {/* <MenuCard dishes={this.state.dishes} />  */}
+//    <TravelMenu /> 
+
+// {/* 
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1 className="App-title">Welcome to React</h1>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+
+
+//     </div>
+//   );
+// }
+// }
+
+
+
+ 
+
+
